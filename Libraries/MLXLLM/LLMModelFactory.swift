8aaf53f5311e8619a07b9a5a727a6ddb5d610f99
Damien <131157132+damienclr@users.noreply.github.com>
2025-03-27 13:02:57 +0100

    Update LLMModelFactory.swift

--- Libraries/MLXLLM/LLMModelFactory.swift
+++ Libraries/MLXLLM/LLMModelFactory.swift
@@ -1,12 +1,8 @@
-// Copyright © 2024 Apple Inc.
-
 import Foundation
-import Hub
 import MLX
 import MLXLMCommon
 import Tokenizers
 
-/// Creates a function that loads a configuration file and instantiates a model with the proper configuration
 private func create<C: Codable, M>(
     _ configurationType: C.Type, _ modelInit: @escaping (C) -> M
 ) -> (URL) throws -> M {
@@ -17,15 +13,9 @@ private func create<C: Codable, M>(
     }
 }
 
-/// Registry of model type, e.g 'llama', to functions that can instantiate the model from configuration.
-///
-/// Typically called via ``LLMModelFactory/load(hub:configuration:progressHandler:)``.
 public class LLMTypeRegistry: ModelTypeRegistry, @unchecked Sendable {
-
-    /// Shared instance with default model types.
     public static let shared: LLMTypeRegistry = .init(creators: all())
 
-    /// All predefined model types.
     private static func all() -> [String: @Sendable (URL) throws -> any LanguageModel] {
         [
             "mistral": create(LlamaConfiguration.self, LlamaModel.init),
@@ -42,161 +32,162 @@ public class LLMTypeRegistry: ModelTypeRegistry, @unchecked Sendable {
             "internlm2": create(InternLM2Configuration.self, InternLM2Model.init),
         ]
     }
-
 }
 
-/// Registry of models and any overrides that go with them, e.g. prompt augmentation.
-/// If asked for an unknown configuration this will use the model/tokenizer as-is.
-///
-/// The python tokenizers have a very rich set of implementations and configuration.  The
-/// swift-tokenizers code handles a good chunk of that and this is a place to augment that
-/// implementation, if needed.
 public class LLMRegistry: AbstractModelRegistry, @unchecked Sendable {
-
-    /// Shared instance with default model configurations.
     public static let shared = LLMRegistry(modelConfigurations: all())
 
     static public let smolLM_135M_4bit = ModelConfiguration(
-        id: "mlx-community/SmolLM-135M-Instruct-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/smolLM_135M_4bit"),
+        name: "SmolLM-135M-Instruct-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "Tell me about the history of Spain."
     )
 
     static public let mistralNeMo4bit = ModelConfiguration(
-        id: "mlx-community/Mistral-Nemo-Instruct-2407-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/mistralNeMo4bit"),
+        name: "Mistral-Nemo-Instruct-2407-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "Explain quaternions."
     )
 
     static public let mistral7B4bit = ModelConfiguration(
-        id: "mlx-community/Mistral-7B-Instruct-v0.3-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/mistral7B4bit"),
+        name: "Mistral-7B-Instruct-v0.3-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "Describe the Swift language."
     )
 
     static public let codeLlama13b4bit = ModelConfiguration(
-        id: "mlx-community/CodeLlama-13b-Instruct-hf-4bit-MLX",
+        source: .url("https://appcaptainshot.frenchpavillon.com/codeLlama13b4bit"),
+        name: "CodeLlama-13b-Instruct-hf-4bit-MLX",
         overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "func sortArray(_ array: [Int]) -> String { <FILL_ME> }"
     )
 
     static public let deepSeekR1_7B_4bit = ModelConfiguration(
-        id: "mlx-community/DeepSeek-R1-Distill-Qwen-7B-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/deepSeekR1_7B_4bit"),
+        name: "DeepSeek-R1-Distill-Qwen-7B-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "Is 9.9 greater or 9.11?"
     )
 
     static public let phi4bit = ModelConfiguration(
-        id: "mlx-community/phi-2-hf-4bit-mlx",
-        // https://www.promptingguide.ai/models/phi-2
+        source: .url("https://appcaptainshot.frenchpavillon.com/phi4bit"),
+        name: "phi-2-hf-4bit-mlx",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "Why is the sky blue?"
     )
 
     static public let phi3_5_4bit = ModelConfiguration(
-        id: "mlx-community/Phi-3.5-mini-instruct-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/phi3_5_4bit"),
+        name: "Phi-3.5-mini-instruct-4bit",
         defaultPrompt: "What is the gravity on Mars and the moon?",
         extraEOSTokens: ["<|end|>"]
     )
 
     static public let phi3_5MoE = ModelConfiguration(
-        id: "mlx-community/Phi-3.5-MoE-instruct-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/phi3_5MoE"),
+        name: "Phi-3.5-MoE-instruct-4bit",
         defaultPrompt: "What is the gravity on Mars and the moon?",
         extraEOSTokens: ["<|end|>"]
-    ) {
-        prompt in
-        "<|user|>\n\(prompt)<|end|>\n<|assistant|>\n"
-    }
+    )
 
     static public let gemma2bQuantized = ModelConfiguration(
-        id: "mlx-community/quantized-gemma-2b-it",
+        source: .url("https://appcaptainshot.frenchpavillon.com/gemma2bQuantized"),
+        name: "quantized-gemma-2b-it",
         overrideTokenizer: "PreTrainedTokenizer",
-        // https://www.promptingguide.ai/models/gemma
-        defaultPrompt: "what is the difference between lettuce and cabbage?"
+        defaultPrompt: "What is the difference between lettuce and cabbage?"
     )
 
     static public let gemma_2_9b_it_4bit = ModelConfiguration(
-        id: "mlx-community/gemma-2-9b-it-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/gemma_2_9b_it_4bit"),
+        name: "gemma-2-9b-it-4bit",
         overrideTokenizer: "PreTrainedTokenizer",
-        // https://www.promptingguide.ai/models/gemma
         defaultPrompt: "What is the difference between lettuce and cabbage?"
     )
 
     static public let gemma_2_2b_it_4bit = ModelConfiguration(
-        id: "mlx-community/gemma-2-2b-it-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/gemma_2_2b_it_4bit"),
+        name: "gemma-2-2b-it-4bit",
         overrideTokenizer: "PreTrainedTokenizer",
-        // https://www.promptingguide.ai/models/gemma
         defaultPrompt: "What is the difference between lettuce and cabbage?"
     )
 
     static public let qwen205b4bit = ModelConfiguration(
-        id: "mlx-community/Qwen1.5-0.5B-Chat-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/qwen205b4bit"),
+        name: "Qwen1.5-0.5B-Chat-4bit",
         overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "why is the sky blue?"
     )
 
     static public let qwen2_5_7b = ModelConfiguration(
-        id: "mlx-community/Qwen2.5-7B-Instruct-4bit",
-        defaultPrompt: "Why is the sky blue?"
-    )
-
-    static public let qwen2_5_1_5b = ModelConfiguration(
-        id: "mlx-community/Qwen2.5-1.5B-Instruct-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/qwen2_5_7b"),
+        name: "Qwen2.5-7B-Instruct-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "Why is the sky blue?"
     )
 
     static public let openelm270m4bit = ModelConfiguration(
-        id: "mlx-community/OpenELM-270M-Instruct",
-        // https://huggingface.co/apple/OpenELM
+        source: .url("https://appcaptainshot.frenchpavillon.com/openelm270m4bit"),
+        name: "OpenELM-270M-Instruct",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "Once upon a time there was"
     )
 
     static public let llama3_1_8B_4bit = ModelConfiguration(
-        id: "mlx-community/Meta-Llama-3.1-8B-Instruct-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/llama3_1_8B_4bit"),
+        name: "Meta-Llama-3.1-8B-Instruct-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "What is the difference between a fruit and a vegetable?"
     )
 
     static public let llama3_8B_4bit = ModelConfiguration(
-        id: "mlx-community/Meta-Llama-3-8B-Instruct-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/llama3_8B_4bit"),
+        name: "Meta-Llama-3-8B-Instruct-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "What is the difference between a fruit and a vegetable?"
     )
 
     static public let llama3_2_1B_4bit = ModelConfiguration(
-        id: "mlx-community/Llama-3.2-1B-Instruct-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com/llama3_2_1B_4bit"),
+        name: "Llama-3.2-1B-Instruct-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "What is the difference between a fruit and a vegetable?"
     )
 
     static public let llama3_2_3B_4bit = ModelConfiguration(
-        id: "mlx-community/Llama-3.2-3B-Instruct-4bit",
+        source: .url("https://appcaptainshot.frenchpavillon.com"),
+        name: "Llama-3.2-3B-Instruct-4bit",
+        overrideTokenizer: "PreTrainedTokenizer",
         defaultPrompt: "What is the difference between a fruit and a vegetable?"
     )
 
     private static func all() -> [ModelConfiguration] {
         [
+            smolLM_135M_4bit,
+            mistralNeMo4bit,
+            mistral7B4bit,
             codeLlama13b4bit,
             deepSeekR1_7B_4bit,
-            gemma2bQuantized,
-            gemma_2_2b_it_4bit,
-            gemma_2_9b_it_4bit,
-            llama3_1_8B_4bit,
-            llama3_2_1B_4bit,
-            llama3_2_3B_4bit,
-            llama3_8B_4bit,
-            mistral7B4bit,
-            mistralNeMo4bit,
-            openelm270m4bit,
-            phi3_5MoE,
-            phi3_5_4bit,
             phi4bit,
+            phi3_5_4bit,
+            phi3_5MoE,
+            gemma2bQuantized,
+            gemma_2_9b_it_4bit,
+            gemma_2_2b_it_4bit,
             qwen205b4bit,
             qwen2_5_7b,
-            qwen2_5_1_5b,
-            smolLM_135M_4bit,
+            openelm270m4bit,
+            llama3_1_8B_4bit,
+            llama3_8B_4bit,
+            llama3_2_1B_4bit,
+            llama3_2_3B_4bit
         ]
     }
-
 }
 
-@available(*, deprecated, renamed: "LLMRegistry", message: "Please use LLMRegistry directly.")
-public typealias ModelRegistry = LLMRegistry
-
 private struct LLMUserInputProcessor: UserInputProcessor {
-
     let tokenizer: Tokenizer
     let configuration: ModelConfiguration
 
@@ -212,8 +203,6 @@ private struct LLMUserInputProcessor: UserInputProcessor {
                 messages: messages, tools: input.tools, additionalContext: input.additionalContext)
             return LMInput(tokens: MLXArray(promptTokens))
         } catch {
-            // #150 -- it might be a TokenizerError.chatTemplate("No chat template was specified")
-            // but that is not public so just fall back to text
             let prompt = input.prompt
                 .asMessages()
                 .compactMap { $0["content"] as? String }
@@ -224,57 +213,39 @@ private struct LLMUserInputProcessor: UserInputProcessor {
     }
 }
 
-/// Factory for creating new LLMs.
-///
-/// Callers can use the `shared` instance or create a new instance if custom configuration
-/// is required.
-///
-/// ```swift
-/// let modelContainer = try await LLMModelFactory.shared.loadContainer(
-///     configuration: ModelRegistry.llama3_8B_4bit)
-/// ```
 public class LLMModelFactory: ModelFactory {
-
     public init(typeRegistry: ModelTypeRegistry, modelRegistry: AbstractModelRegistry) {
         self.typeRegistry = typeRegistry
         self.modelRegistry = modelRegistry
     }
 
-    /// Shared instance with default behavior.
     public static let shared = LLMModelFactory(
         typeRegistry: LLMTypeRegistry.shared, modelRegistry: LLMRegistry.shared)
 
-    /// registry of model type, e.g. configuration value `llama` -> configuration and init methods
     public let typeRegistry: ModelTypeRegistry
-
-    /// registry of model id to configuration, e.g. `mlx-community/Llama-3.2-3B-Instruct-4bit`
     public let modelRegistry: AbstractModelRegistry
 
     public func _load(
-        hub: HubApi, configuration: ModelConfiguration,
+        configuration: ModelConfiguration,
         progressHandler: @Sendable @escaping (Progress) -> Void
     ) async throws -> ModelContext {
-        // download weights and config
         let modelDirectory = try await downloadModel(
-            hub: hub, configuration: configuration, progressHandler: progressHandler)
+            configuration: configuration, progressHandler: progressHandler)
 
-        // load the generic config to unerstand which model and how to load the weights
-        let configurationURL = modelDirectory.appending(component: "config.json")
+        let configurationURL = modelDirectory.appending(component: "model.json")
         let baseConfig = try JSONDecoder().decode(
             BaseConfiguration.self, from: Data(contentsOf: configurationURL))
         let model = try typeRegistry.createModel(
             configuration: configurationURL, modelType: baseConfig.modelType)
 
-        // apply the weights to the bare model
         try loadWeights(
             modelDirectory: modelDirectory, model: model, quantization: baseConfig.quantization)
 
-        let tokenizer = try await loadTokenizer(configuration: configuration, hub: hub)
+        let tokenizer = try await loadTokenizer(configuration: configuration)
 
         return .init(
             configuration: configuration, model: model,
             processor: LLMUserInputProcessor(tokenizer: tokenizer, configuration: configuration),
             tokenizer: tokenizer)
     }
-
 }
